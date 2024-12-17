c48334509148120b034485a26e4a26f27fcc1ae6
Alexander Six <alexanderhsix@gmail.com>
2024-12-16 20:35:02 -0500

    Add new git-based pane for nvim dashboard in lazy-nvim

--- lazy-nvim/README.md
+++ lazy-nvim/README.md
@@ -2,3 +2,11 @@
 
 A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+
+## Requirements
+
+### Dashboard
+
+- `gh` command line tool
+- `gh-notify` command line extension
+  - `gh ext install meiji163/gh-notify`
--- lazy-nvim/lua/plugins/snacks.lua
+++ lazy-nvim/lua/plugins/snacks.lua
@@ -2,5 +2,88 @@ return {
   "snacks.nvim",
   opts = {
     scroll = { enabled = false },
+    dashboard = {
+      sections = {
+        { section = "header" },
+        {
+          pane = 2,
+          section = "terminal",
+          cmd = "",
+          height = 5,
+          padding = 1,
+        },
+        {
+          section = "keys",
+          gap = 1,
+          padding = 1,
+        },
+        {
+          pane = 2,
+          icon = " ",
+          desc = "Browse Repo",
+          padding = 1,
+          key = "b",
+          action = function()
+            Snacks.gitbrowse()
+          end,
+        },
+        function()
+          local in_git = Snacks.git.get_root() ~= nil
+          local flipped_path = string.reverse(Snacks.git.get_root())
+          local slash_index, _ = string.find(flipped_path, "/", 0, true) or 0, 0
+          local repo_name = string.reverse(string.sub(flipped_path, 1, slash_index - 1))
+          local cmds = {
+            {
+              title = "Notifications",
+              cmd = "gh notify -s -a -n5 -f '" .. repo_name .. "'",
+              action = function()
+                vim.ui.open("https://github.com/notifications")
+              end,
+              key = "n",
+              icon = " ",
+              height = 5,
+              enabled = true,
+            },
+            {
+              title = "Open Issues",
+              cmd = "gh issue list -L 3",
+              key = "i",
+              action = function()
+                vim.fn.jobstart("gh issue list --web", { detach = true })
+              end,
+              icon = " ",
+              height = 7,
+            },
+            {
+              icon = " ",
+              title = "Open PRs",
+              cmd = "gh pr list -L 3",
+              key = "p",
+              action = function()
+                vim.fn.jobstart("gh pr list --web", { detach = true })
+              end,
+              height = 7,
+            },
+            {
+              icon = " ",
+              title = "Git Status",
+              cmd = "git --no-pager diff --stat -B -M -C",
+              height = 10,
+            },
+          }
+          return vim.tbl_map(function(cmd)
+            return vim.tbl_extend("force", {
+              pane = 2,
+              section = "terminal",
+              enabled = in_git,
+              padding = 1,
+              ttl = 5 * 60,
+              indent = 3,
+            }, cmd)
+          end, cmds)
+        end,
+        { section = "startup" },
+      },
+    },
   },
 }
