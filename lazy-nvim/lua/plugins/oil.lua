d8d1bece36fecb2ce71a66ffd6d8d83e4a690aa3
Alexander Six <alexanderhsix@gmail.com>
2025-03-28 08:28:26 -0400

    Replace oil with Snacks

--- lazy-nvim/lua/plugins/oil.lua
+++ lazy-nvim/lua/plugins/oil.lua
@@ -4,19 +4,19 @@ return {
     enabled = false,
   },
 
-  {
-    "stevearc/oil.nvim",
-    opts = {},
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    keys = {
-      { "<leader>e", "<cmd>Oil<cr>" },
-    },
-    config = function()
-      require("oil").setup({
-        view_options = {
-          show_hidden = true,
-        },
-      })
-    end,
-  },
+  -- {
+  --   "stevearc/oil.nvim",
+  --   opts = {},
+  --   dependencies = { "nvim-tree/nvim-web-devicons" },
+  --   keys = {
+  --     { "<leader>e", "<cmd>Oil<cr>" },
+  --   },
+  --   config = function()
+  --     require("oil").setup({
+  --       view_options = {
+  --         show_hidden = true,
+  --       },
+  --     })
+  --   end,
+  -- },
 }
--- lazy-nvim/lua/plugins/snacks.lua
+++ lazy-nvim/lua/plugins/snacks.lua
@@ -1,9 +1,24 @@
 return {
   "snacks.nvim",
+  keys = {
+    {
+      "<leader>e",
+      function()
+        Snacks.explorer()
+      end,
+    },
+  },
   opts = {
     picker = {
       matcher = {
         frecency = true,
+      },
+      sources = {
+        explorer = {
+          auto_close = true,
+          layout = { layout = { position = "right" }, preset = "sidebar", preview = false },
+          matcher = { fuzzy = true },
+        },
       },
     },
     scroll = { enabled = false },
